8bf827ff0586ad34e84daaed7c3021977eb774ac
Allen Chrisso Dhas <allencdvja@gmail.com>
2024-09-03 23:09:25 +0530

    padding header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,9 @@
-
 import React from 'react';
 
 
 export default function Header() {
     return (
-        <div className=" mx-10 py-10 flex justify-between">
+        <div className=" mx-10 py-6 flex justify-between">
             <p className="text-white border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30 font-semibold">
                 CollabX
             </p>
